f0651aaf56171575aef0ae04aab4103c4e82981c
Mohammad Bashir <tukurmohammad8@gmail.com>
2024-09-14 22:00:25 +0100

    Practiced on factory functions and closures

--- Pactice-Code/script.js
+++ Pactice-Code/script.js
@@ -1,3 +1,92 @@
+//Factory Functions
+function createUser(name) {
+  const discordName = "@" + name.toLowerCase();
+
+  let reputation = 0;
+  const getReputation = () => reputation;
+  const giveReputation = () => reputation++;
+  const removeReputation = () => reputation--;
+
+  return { name, discordName, getReputation, giveReputation, removeReputation };
+}
+
+const josh = createUser("josh");
+josh.giveReputation();
+josh.giveReputation();
+
+console.log({
+  discordName: josh.discordName,
+  reputation: josh.getReputation(),
+});
+
+const mohammad = createUser("Mohammad");
+mohammad.giveReputation();
+mohammad.giveReputation();
+// mohammad.removeReputation();
+
+console.log({
+  discordName: mohammad.discordName,
+  currentReputation: mohammad.getReputation(),
+});
+
+function createPlayer(name, level) {
+  const { getReputation, giveReputation, discordName } = createUser(name);
+
+  const increaseLevel = () => level++;
+  return { name, discordName, getReputation, giveReputation, increaseLevel };
+}
+
+const bashir = createPlayer("Bashir", 200);
+bashir.giveReputation();
+console.log({
+  discordName: bashir.discordName,
+  Reps: bashir.giveReputation(),
+});
+
+const calculator = (function () {
+  const add = (a, b) => a + b;
+  const sub = (a, b) => a - b;
+  const div = (a, b) => a / b;
+  const mul = (a, b) => a * b;
+
+  return { add, sub, div, mul };
+})();
+
+console.log(calculator.add(3, 5));
+console.log(calculator.sub(6, 2));
+console.log(calculator.mul(14, 5534));
+
+// logs { discordName: "@josh", reputation: 2 }
+
+/**
+function makeAdding(firstNumber) {
+  // "first" is scoped within the makeAdding function
+  const first = firstNumber;
+  return function resulting(secondNumber) {
+    // "second" is scoped within the resulting function
+    const second = secondNumber;
+    return function third(thirdNumber) {
+      return first + second + thirdNumber;
+    };
+  };
+}
+// but we've not seen an example of a "function"
+// being returned, thus far - how do we use it?
+
+const add5 = makeAdding(5);
+const secAdd = add5(2);
+console.log(secAdd(3)); // logs 7
+
+// function loopAdd(num) {
+//   let sum = 0;
+//   for (let i = 1; i <= num; i++) {
+//     sum = sum + i;
+//   }
+//   return sum;
+// }
+ */
+// console.log(loopAdd(5));
+/**
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -53,3 +142,4 @@ const player2 = new Player("also steve", "O");
 
 player1.sayName(); // Hello, I'm steve!
 player2.sayName(); // Hello, I'm also steve!
+ */
