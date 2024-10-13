e190a61ae4fdfb9066e74632c959502516bb0981
Mohammad Bashir <tukurmohammad8@gmail.com>
2024-10-13 17:50:53 +0100

    Created a weather app

---
+++ weather-app/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true, // Cleans the output folder before each build
+  },
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Weather App",
+      template: "./index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  mode: "development",
+};
