0e8bf771dc1b41990a395bc4da22212b28366a75
Mohammad Tukur <tukurmohammad8@gmail.com>
2025-05-07 17:06:58 +0100

    Changed root back to App

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import Greetings from "./components/Greetings.jsx";
+// import Greetings from "./components/Greetings.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Greetings />
+    <App />
   </StrictMode>
 );
