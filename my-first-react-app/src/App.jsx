d9fd00f8767c2287c4a958c364be9c2e1a20050e
Mohammad Tukur <tukurmohammad8@gmail.com>
2025-05-08 10:43:17 +0100

    Added TODO Lists

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -24,30 +24,63 @@ function List(props) {
   //   </>
   // );
   // OR
-  return (
-    <>
-      {!props.animalsList && <div>Loading...</div>}
-      {props.animalsList && props.animalsList.length > 0 && (
-        <ul>
-          {props.animalsList.map((animal) => {
-            return <li key={animal}>{animal}</li>;
-          })}
-        </ul>
-      )}
-      {props.animalsList && props.animalsList.length === 0 && (
-        <div>There are no animals in the list!</div>
-      )}
-    </>
-  );
+  if (!props.listType) {
+    return "No List Type";
+  }
+
+  if (props.listType == "animals") {
+    return (
+      <>
+        {!props.animalsList && <div>Loading...</div>}
+        {props.animalsList && props.animalsList.length > 0 && (
+          <ul>
+            {props.animalsList.map((animal) => {
+              return <li key={animal}>{animal}</li>;
+            })}
+          </ul>
+        )}
+        {props.animalsList && props.animalsList.length === 0 && (
+          <div>There are no animals in the list!</div>
+        )}
+      </>
+    );
+  } else if (props.listType == "todos") {
+    return (
+      <>
+        {!props.todoList && <div>Loading...</div>}
+        {props.todoList && props.todoList.length > 0 && (
+          <ul>
+            {props.todoList.map((todo) => {
+              return <li key={todo.id}>{todo.task}</li>;
+            })}
+          </ul>
+        )}
+        {props.todoList && props.todoList.length === 0 && (
+          <div>There are no todos in the list!</div>
+        )}
+      </>
+    );
+  }
 }
 function App() {
   const animals = ["Lion", "Cow", "Snake", "Lizard"];
   // const animals = [];
+  const todos = [
+    { task: "mow the yard", id: crypto.randomUUID() },
+    { task: "Work on Odin Projects", id: crypto.randomUUID() },
+    { task: "feed the cat", id: crypto.randomUUID() },
+  ];
 
   return (
     <>
-      <h1>Animals: </h1>
-      <List animalsList={animals} />
+      <div>
+        <h1>Animals: </h1>
+        <List animalsList={animals} listType={"animals"} />
+      </div>
+      <div>
+        <h1>TODO List</h1>
+        <List todoList={todos} listType={"todos"} />
+      </div>
     </>
   );
 }
